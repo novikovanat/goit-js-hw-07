2d5a13473e2328a10fd94394679e9f52399a03a3
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-02-08 19:51:07 +0100

    task-3 finished

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,18 @@
-const nameInput = donument.querrySelector('.name-input');
-const nameOutput = document.querySelector('.name-output');
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+console.log(nameInput);
+console.log(nameOutput);
 
-nameInput.addEventLissener(input, showName);
+nameInput.addEventListener('input', showName);
 
-function showName(event) {}
+function showName(event) {
+  console.log(event);
+  const nameInputTrimmed = nameInput.value.trim();
+  console.log(`this is full string "${nameInput.value}"`);
+  console.log(`this is trimmed string "${nameInputTrimmed}"`);
+  console.log(nameInputTrimmed === '');
+  nameInputTrimmed === ''
+    ? console.log('this is empty string')
+    : (nameOutput.textContent = nameInputTrimmed);
+}
+nameInput.addEventListener('blur', () => (nameInput.value = ''));

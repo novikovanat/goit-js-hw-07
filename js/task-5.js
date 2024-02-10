2597f282a2ba8c8eb8c1b444e6f77472e5bc7bc5
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-02-10 19:35:23 +0100

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorName = document.querySelector('.color');
+const body = document.querySelector('body');
+const button = document.querySelector('.change-color');
+
+button.addEventListener('click', changeColor);
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  colorName.innerText = body.style.backgroundColor;
+}

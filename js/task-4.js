0e75102f76d51af254dd6e8d028a2c541ffb9a8f
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-02-10 19:05:10 +0100

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', checkAndSubmit);
+
+let credentials = {};
+function checkAndSubmit(event) {
+  event.preventDefault();
+  const login = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+
+  if (password.trim() === '' || login.trim() === '') {
+    alert('All form fields must be filled in');
+  } else {
+    credentials.login = login;
+    credentials.password = password;
+    console.log(credentials);
+  }
+
+  form.reset();
+}

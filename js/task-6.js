987be28a61100fa6f5a57c0978d82c3618c261ef
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-02-10 19:56:17 +0100

    task6 started

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+
+createBtn.addEventListener('click', createBoxes());
+destroyBtn.addEventListener('click', destroyBoxes());
+
+function createBoxes(amount) {}
+function destroyBoxes() {}

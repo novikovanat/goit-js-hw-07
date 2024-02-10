b2b6a6449f7bacace4e6a975d60fbda1c88a9bb2
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-02-10 20:37:45 +0100

    number check added

--- js/task-6.js
+++ js/task-6.js
@@ -6,9 +6,18 @@ function getRandomHexColor() {
 
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
+const numberInput = document.querySelector('input[type="number"]');
 
 createBtn.addEventListener('click', createBoxes());
 destroyBtn.addEventListener('click', destroyBoxes());
+numberInput.addEventListener('change', getANumber);
 
+let number;
+function getANumber() {
+  numberInput.value > 0 && numberInput.value < 100
+    ? (number = numberInput.value)
+    : alert('Insert number between 1 and 100');
+  console.log(number);
+}
 function createBoxes(amount) {}
 function destroyBoxes() {}

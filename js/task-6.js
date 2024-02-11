cef3a9432c62e9ee1a2e2c3a4916519fe1a48b83
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-02-11 16:02:50 +0100

    stage

--- js/task-6.js
+++ js/task-6.js
@@ -6,34 +6,49 @@ function getRandomHexColor() {
 
 const createBtn = document.querySelector('button[data-create]');
 const destroyBtn = document.querySelector('button[data-destroy]');
-const number = document.querySelector('input[type="number"]');
+const numberStr = document.querySelector('input[type="number"]');
 const boxes = document.getElementById('boxes');
 
-createBtn.addEventListener('click', createBoxes);
+createBtn.addEventListener('click', createHandler);
 destroyBtn.addEventListener('click', destroyBoxes);
-number.addEventListener('input', getANumber);
-number.addEventListener('blur', wipe);
+numberStr.addEventListener('change', getANumber);
+// number.addEventListener('blur', wipe);
 
 function wipe() {
   number.value = '';
 }
 
-function getANumber() {
-  let numberString = number.value;
-  let amount = Number(numberString);
-  console.log(amount);
-  if (Number(amount) > 0 && Number(amount) < 100) {
-    return Number(amount);
-  } else {
-    console.log('Insert number between 1 and 100');
-    return (amount = 0);
-  }
+function createHandler() {
+  console.log(createBoxes(amount));
+  amount = 0;
 }
 
-console.log(getANumber());
-function createBoxes(amount) {}
+let amount;
+function getANumber() {
+  let number = parseInt(numberStr.value);
+  numberStr.value = '';
+  if (number > 0 && number < 100) {
+    amount = number;
+  } else {
+    console.log('Insert number between 1 and 100');
+  }
+}
+// Функція має створювати стільки <div> елементів, скільки вказано в параметрі amount і додавати їх у DOM дочірніми елементами для div#boxes.
 
-function destroyBoxes() {}
+// Розміри першого <div> елемента мають бути 30px на 30px.
+// Кожен наступний елемент повинен бути ширшим і вищим від попереднього на 10px.
+// Усі елементи повинні мати випадковий колір фону. Використовуй готову функцію getRandomHexColor() для отримання випадкового кольору.
+
+function createBoxes(amount) {
+  console.log(`I'm create boxes ${amount}`);
+
+  let string = `<div class = 'container' slyle = "width = 200px" ><div style = "background-color: red; width :30px ; height: 30px ; "> this is empty div</div></div>`;
+  boxes.insertAdjacentHTML('beforeend', string);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
 
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215)

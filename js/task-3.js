2beb73b73c2f4444f353f92653e7f477ddaf482d
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-02-13 09:29:16 +0100

    coments deleted

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,11 @@
 const nameInput = document.getElementById('name-input');
 const nameOutput = document.getElementById('name-output');
-console.log(nameInput);
-console.log(nameOutput);
 
 nameInput.addEventListener('input', showName);
 
 function showName(event) {
-  console.log(event);
   const nameInputTrimmed = nameInput.value.trim();
-  console.log(`this is full string "${nameInput.value}"`);
-  console.log(`this is trimmed string "${nameInputTrimmed}"`);
-  console.log(nameInputTrimmed === '');
+
   nameInputTrimmed === ''
     ? console.log('this is empty string')
     : (nameOutput.textContent = nameInputTrimmed);

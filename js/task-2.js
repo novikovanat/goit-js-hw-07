e4ef3cc6e14007771d658af4a6851525b6015bfd
novikovanat <72397136+novikovanat@users.noreply.github.com>
2024-02-13 22:09:56 +0100

    array cuted

--- js/task-2.js
+++ js/task-2.js
@@ -11,21 +11,9 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-  {
-    url: 'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Elephant Beside on Baby Elephant',
-  },
-  {
-    url: 'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Blue Geeen and Orange Parrot',
-  },
-  {
-    url: 'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Zebras on Zebra',
-  },
 ];
 
-const string = images.slice(0, 3).reduce((previousString, image) => {
+const string = images.reduce((previousString, image) => {
   return (
     previousString +
     `<li class="gallery-item"><img class ="img" src="${image.url}" alt="${image.alt}"></li>`
